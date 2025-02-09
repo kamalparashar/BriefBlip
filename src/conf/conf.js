95e3e511397a92fff742ebc40410b01c087cf5aa
Kamal Parashar <kamalparasharp1123@gmail.com>
2025-02-09 20:46:40 +0530

    some changes

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,7 +1,6 @@
 const conf = {
     nhost_region: String(import.meta.env.VITE_NHOST_REGION),
     nhost_subdomain: String(import.meta.env.VITE_NHOST_SUBDOMAIN),
-    n8n_url: String(import.meta.env.VITE_N8N_URL),
     hasura_url: String(import.meta.env.VITE_HASURA_URL),
     hasura_secret: String(import.meta.env.VITE_HASURA_SECRET)
 }
